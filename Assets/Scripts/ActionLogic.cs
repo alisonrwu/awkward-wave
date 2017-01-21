d9a61e34c04e8427e806861ca6bb4acd9a578c02
Cherry <d6m8@ugrad.cs.ubc.ca>
2017-01-21 15:49:29 -0800

    some raycasting script

---
+++ Assets/Scripts/ActionLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionLogic : MonoBehaviour {
+
+    public Transform sightStart, sightEnd;
+    public bool spotted = false; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Raycasting();
+	}
+
+    void Raycasting() {
+        Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
+        spotted = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+    }
+
+}
