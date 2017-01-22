d5850d157b63558dd8947ed78c0991d71836e4b9
ZHUALICE <alice-desu@hotmail.com>
2017-01-21 17:35:20 -0800

    Added game over scene, animations, and death when awkward health is low

---
+++ Assets/Scripts/RunningAll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningAll : MonoBehaviour {
+    // Use this for initialization
+    public float dir;
+    public float flyDir;
+
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //   Vector3 position = this.transform.position;
+        //   position.x -= 0.05f;
+        //   this.transform.position = position;
+        RunDirection(dir);
+        FlyDirection(flyDir);
+    }
+
+
+    void RunDirection (float dir)
+    {
+        Vector3 position = this.transform.position;
+        position.x += dir;
+        this.transform.position = position;
+    }
+
+
+    void FlyDirection(float flyDir)
+    {
+        Vector3 position = this.transform.position;
+        position.y += dir;
+        this.transform.position = position;
+    }
+
+}
