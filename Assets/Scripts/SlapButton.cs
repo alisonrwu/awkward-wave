83e331920ece22dc867c8698476d21eaebc0beee
Cherry <d6m8@ugrad.cs.ubc.ca>
2017-01-21 17:56:52 -0800

    some changes to make ken fall

---
+++ Assets/Scripts/SlapButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SlapButton : MonoBehaviour
+{
+    private Button button;
+    public GameObject angry;
+
+    //public AwkwardController ac;
+
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        button.onClick.AddListener(() => Clicked());
+
+    }
+
+    void Clicked()
+    {
+        angry = GameObject.Find("Angry");
+        if (angry = GameObject.FindGameObjectWithTag("Angry"))
+        {
+            angry.transform.Translate(Vector3.down);
+            //ac.increaseScore();
+        }
+        else
+        {
+            //ac.lowerAwkwardHealth();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
